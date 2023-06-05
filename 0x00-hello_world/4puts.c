2c60bbc35d8e964fc1e13db6b56b3ebaf922dd83
MutuohoroCS <mchegesammy@gmail.com>
2023-06-05 04:28:32 -0700

    add solution for task 4

---
+++ 0x00-hello_world/4puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print string or something
+ * Return: 0 if code is kidu successful
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
