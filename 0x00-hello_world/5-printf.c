383e3b5c9652f2ce1e91af1e8e2540f59d4748d0
MutuohoroCS <mchegesammy@gmail.com>
2023-06-05 04:38:21 -0700

    add solution for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -print string damit
+ * Return: 0 if code kipchoges
+ * */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
