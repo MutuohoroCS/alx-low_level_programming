6d3d54b03ad2f0524bacf310a85df4b30016b832
MutuohoroCS <mchegesammy@gmail.com>
2023-09-11 20:14:59 +0000

    add solution for task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+/**
+ * main - print sizes of intrinsic data types
+ * Return: 0 when code kipchoges
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	int num;
+	long int longnum;
+	long long int longlongnum;
+	float flt;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+
+	return (0);
+}
