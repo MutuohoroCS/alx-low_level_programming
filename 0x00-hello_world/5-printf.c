7d2fb69c3528cf01e17eb3edfd86721f03240fa3
MutuohoroCS <mchegesammy@gmail.com>
2023-09-11 19:48:29 +0000

    add solution for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - print a string
+ * Return: 0 when run wizaut error
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
