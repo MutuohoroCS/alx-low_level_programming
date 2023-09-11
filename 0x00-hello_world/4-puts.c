fc332a610f7830e3d9cc24c0125f35c03d395c8b
MutuohoroCS <mchegesammy@gmail.com>
2023-09-11 19:38:24 +0000

    add solution for task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main -prints a string
+ * Return: 0 when string is printed
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
