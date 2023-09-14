f824243234c0b616da697cbfc14d8e283a3f8f8f
MutuohoroCS <mchegesammy@gmail.com>
2023-09-14 01:29:14 -0700

    add solution for task 4

---
+++ 0x01-variables_if_else_while/4-print_alphbt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet and ignore q and e
+ * Return: 0 always when success
+ */
+
+int main(void)
+{
+	char lettr;
+
+	lettr = 'a' - 1;
+
+	while (lettr <= 'z')
+	{
+		lettr++;
+		if (lettr == 'q' || lettr == 'e')
+		{
+			continue;
+		}
+		putchar(lettr);
+	}
+	putchar('\n');
+
+	return (0);
+}
