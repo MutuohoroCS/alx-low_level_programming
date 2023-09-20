b092890594ab19954294ef4f86aa0b51808bbc25
MutuohoroCS <mchegesammy@gmail.com>
2023-09-20 03:10:52 +0000

    add solution for task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 lines of alphabet in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int lines;
+
+	lines = 1;
+	while (lines <= 10)
+	{
+		char lttr = 'a';
+
+		while (lttr <= 'z')
+		{
+			_putchar(lttr);
+			lttr++;
+		}
+		_putchar('\n');
+		lines++;
+	}
+	return;
+}
