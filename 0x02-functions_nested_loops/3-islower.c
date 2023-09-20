86910996a8b5c4ae3469a694c23934dfb5985b50
MutuohoroCS <mchegesammy@gmail.com>
2023-09-20 00:41:11 -0700

    add solution to task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,14 @@
 
 /**
  * _islower - checks whether c is a lowercase letter
- * Return: 0 false
-  */
+ * @c: character to be checked lowercase
+ * Return: 1 if the char is lowercase
+ */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
-	} else
-	{
-		return (0);
 	}
 }
