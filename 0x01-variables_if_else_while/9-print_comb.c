c20cda455d38d6b9dfb8aeed3b8184fead23e878
MutuohoroCS <mchegesammy@gmail.com>
2023-09-21 03:23:12 -0700

    add solution for task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints 0 to 9 but with a twist
+ * Return: 0 success or something idk
+ */
+
 int main(void)
 {
 	int chrctr;
@@ -9,8 +14,11 @@ int main(void)
 	while (chrctr < 10)
 	{
 		putchar(chrctr + '0');
-		putchar(',');
-		putchar(' ');
+		if (chrctr != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		chrctr++;
 	}
 	putchar('\n');
