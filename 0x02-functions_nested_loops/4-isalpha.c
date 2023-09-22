f99cf3742a42612c3b4a45dfba9abd807dd97f59
MutuohoroCS <mchegesammy@gmail.com>
2023-09-22 02:00:32 +0000

    add solution for task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * _isalpha - check whether alphabet is alphabeting both lower and uppercase
+ * @c: this is the one being checked if its alphabeting
+ * Return: 1if true and zero if false duh!
+ */
+
 int _isalpha(int c)
 {
 	c = c + '0';
-	if ((c >= 65 || c <=90) && (c >= 97 || c <= 122))
-		return 1;
-	return 0;
+	if ((c >= 65 || c == 90) && (c >= 97 || c == 122))
+		return (1);
+	return (0);
 }
