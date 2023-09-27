3e2fdeb87a2f39ba51c445d0065d0a8778fde89d
MutuohoroCS <mchegesammy@gmail.com>
2023-09-27 01:23:47 +0000

    add solution for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @num1: skibidi pa pa
+ * @num2: skibidi pa pa
+ * Return: addition of the two numbers
+ */
+
+int add(int num1, int num2)
+{
+	result = num1 + num2;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@ int _abs(int nmbr);
 int print_last_digit(int nmbr);
 void jack_bauer(void);
 void times_table(void);
+int add(int num1, int num2);
