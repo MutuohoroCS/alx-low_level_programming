8aa81735ac25d6f4bf8e5b7f86223fbd1deb4a77
MutuohoroCS <mchegesammy@gmail.com>
2023-09-27 01:09:01 +0000

    add solution for task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -34,7 +34,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) % '0');
+				_putchar((prod % 10) + '0');
 			}
 			times++;
 			_putchar('\n');
